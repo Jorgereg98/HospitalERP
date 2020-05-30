a5d5920d08ce1eec3fd175837a48ad1e75bb7930
Jorge Ruiz <jorgereg1998@gmail.com>
2020-05-30 15:28:44 -0500

    Commit

--- erp.js
+++ erp.js
@@ -3,9 +3,15 @@ var bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) =>{
     res.json({message: "Connected"})
 })
+
+// require("./app/routes/customer.routes.js")
+
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
     console.log("Servicio funcionando");
