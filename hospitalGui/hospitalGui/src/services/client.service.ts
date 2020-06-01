49ee04f24c4f7ad390a56fbc92800a42cb26a055
Luis Fco López Gómez <luisfcolg.13@gmail.com>
2020-05-31 22:06:27 -0500

    - Services for the app

--- hospitalGui/hospitalGui/src/services/client.service.ts
+++ hospitalGui/hospitalGui/src/services/client.service.ts
@@ -9,15 +9,45 @@ import { IClient } from 'src/models/client';
 export class ClientService {
     public url: string = "http://localhost:3000/clients";
 
-    constructor(private httpClient: HttpClient) {
+    constructor(private httpClient:HttpClient) {
     }
 
-    public getClientById(clientId: number) {
-        return this.httpClient.get<IClient>(
-        `${this.url}/${clientId}`, { headers: this.getHeaders() }
-        );
+    public getClients() {
+      return this.httpClient.get<IClient[]>(
+        `${this.url}`,
+        { headers: this.getHeaders() }
+      );
     }
 
+    public getClientById(clientId:number) {
+      return this.httpClient.get<IClient>(
+        `${this.url}/${clientId}`,
+        { headers: this.getHeaders() }
+      );
+    }
+
+    public createClient(client:IClient) {
+      return this.httpClient.post<IClient>(
+        `${this.url}`,
+        client,
+        { headers: this.getHeaders() }
+      );
+    }
+
+    public updateClient(client:IClient, clientId:number) {
+      return this.httpClient.put<IClient>(
+        `${this.url}/${clientId}`,
+        client,
+        { headers: this.getHeaders() }
+      );
+    }
+
+    public deleteClient(clientId:number) {
+      return this.httpClient.delete<IClient>(
+        `${this.url}/${clientId}`,
+        { headers: this.getHeaders() }
+      );
+    }
 
     private getHeaders(): HttpHeaders {
         let headers = new HttpHeaders()
