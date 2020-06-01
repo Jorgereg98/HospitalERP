18cd30ffd8e2c4ead4ef6414149a20ea703e84b7
Luis Fco López Gómez <luisfcolg.13@gmail.com>
2020-06-01 01:38:28 -0500

    - Admin page

--- hospitalGui/hospitalGui/src/app/components/admin/admin.component.ts
+++ hospitalGui/hospitalGui/src/app/components/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core'
+import { IAdmin } from 'src/models/admin';
+import { ActivatedRoute } from '@angular/router';
+import { AdminService } from 'src/services/admin.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
     selector: 'admin',
@@ -6,5 +10,31 @@ import {Component} from '@angular/core'
     styleUrls: ['./admin.component.css']
 })
 export class AdminComponent{
+    title="Admin"
+    page:string;
 
+    public admin: IAdmin = {};
+    public adminId: number;
+
+    constructor(private router: ActivatedRoute, private adminService: AdminService, private httpClient: HttpClient){
+        this.adminId = +this.router.snapshot.paramMap.get('id');
+        console.log(this.adminId)
+        this.loadAdmin();
+    }
+
+    loadAdmin(){
+        this.adminService.getAdminById(this.adminId).toPromise()
+        .then((admin) => {
+            if(admin == null){
+              this.admin = {};
+              console.log("null");
+            }
+            else{
+              this.admin = admin;
+              console.log("not null");
+            }
+            console.log(this.admin);
+            console.log(this.admin.fname);
+        });
+    }
 }
